29a86ffed662c75003ef9e5a87651a26ce6e7431
Danieleleite <danileite08@gmail.com>
2016-03-16 20:43:38 -0300

    Exemplo1

---
+++ pagina1.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+echo "Coding Standards";
+
+$nome = 'Daniele';
+
+$nota = 2 + 6;
+
+$nome_completo = 'Daniele '.'Leite';
+
+$nome_completo .= 'Silva';
+
+echo '<br><br> Nome:'.$nome_completo;
+
+?>
